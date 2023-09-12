bf60e71170af73d25eb4a8af60a26d0c79669c61
Guterresthomas <guterresmikael@gmail.com>
2023-09-12 17:39:42 -0300

    loop

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,22 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("please, type a number!");
+        let guess: u32 = guess.trim().parse().expect("please, type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 
 }
