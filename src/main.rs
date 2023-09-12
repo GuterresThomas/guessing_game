56af24ef9dbeb14d3aceb04d83bc44633e273325
Guterresthomas <guterresmikael@gmail.com>
2023-09-12 19:34:52 -0300

    guessing game working

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     
-    println!("the secret numer is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -19,7 +18,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("please, type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
